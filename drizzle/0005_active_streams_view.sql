c246291586978746475ebbcd4ab34add611603a4
John | Elite Encoder <john@eliteencoder.net>
2026-04-17 19:03:53 -0400

    Revert "Add active streams index and view with Drizzle snapshot 0005."
    This reverts commit 8c93b2c0c9a9faf7725a6dcd3ee2bcee29c20089.

--- drizzle/0005_active_streams_view.sql
+++
@@ -1,2 +0,0 @@
-CREATE INDEX "transactions_usage_confirmed_stream_session_created_at_idx" ON "transactions" USING btree ("stream_session_id","created_at") WHERE "transactions"."type" = 'usage' AND "transactions"."status" = 'confirmed' AND "transactions"."stream_session_id" IS NOT NULL;--> statement-breakpoint
-CREATE VIEW "public"."active_stream_ids_by_recent_payment" AS (SELECT DISTINCT "stream_session_id" AS "id" FROM "transactions" WHERE "type" = 'usage' AND "status" = 'confirmed' AND "stream_session_id" IS NOT NULL AND "created_at"::timestamptz > NOW() - INTERVAL '5 minutes');
